c4949762883ee053845598e97da5ba23521bfbc7
Daniel Damaceno <105954004+danieldamaceno@users.noreply.github.com>
2022-07-17 22:21:16 -0300

    Add files via upload

---
+++ CALCULADORA/Form1.Designer.vb
@@ -0,0 +1,175 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.txtNumero1 = New System.Windows.Forms.TextBox()
+        Me.txtNumero2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblResultado = New System.Windows.Forms.Label()
+        Me.btnSoma = New System.Windows.Forms.Button()
+        Me.btnMenos = New System.Windows.Forms.Button()
+        Me.btnVezes = New System.Windows.Forms.Button()
+        Me.btnDividir = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtNumero1
+        '
+        Me.txtNumero1.Location = New System.Drawing.Point(151, 46)
+        Me.txtNumero1.Name = "txtNumero1"
+        Me.txtNumero1.Size = New System.Drawing.Size(100, 23)
+        Me.txtNumero1.TabIndex = 0
+        '
+        'txtNumero2
+        '
+        Me.txtNumero2.Location = New System.Drawing.Point(151, 86)
+        Me.txtNumero2.Name = "txtNumero2"
+        Me.txtNumero2.Size = New System.Drawing.Size(100, 23)
+        Me.txtNumero2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 49)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(107, 15)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Digite um número:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 92)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(107, 15)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Digite um número:"
+        '
+        'lblResultado
+        '
+        Me.lblResultado.AutoSize = True
+        Me.lblResultado.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblResultado.Location = New System.Drawing.Point(173, 231)
+        Me.lblResultado.Name = "lblResultado"
+        Me.lblResultado.Size = New System.Drawing.Size(23, 25)
+        Me.lblResultado.TabIndex = 4
+        Me.lblResultado.Text = "0"
+        '
+        'btnSoma
+        '
+        Me.btnSoma.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnSoma.Location = New System.Drawing.Point(29, 139)
+        Me.btnSoma.Name = "btnSoma"
+        Me.btnSoma.Size = New System.Drawing.Size(75, 23)
+        Me.btnSoma.TabIndex = 5
+        Me.btnSoma.Text = "SOMA"
+        Me.btnSoma.UseVisualStyleBackColor = True
+        '
+        'btnMenos
+        '
+        Me.btnMenos.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnMenos.Location = New System.Drawing.Point(109, 139)
+        Me.btnMenos.Name = "btnMenos"
+        Me.btnMenos.Size = New System.Drawing.Size(75, 23)
+        Me.btnMenos.TabIndex = 6
+        Me.btnMenos.Text = "MENOS"
+        Me.btnMenos.UseVisualStyleBackColor = True
+        '
+        'btnVezes
+        '
+        Me.btnVezes.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnVezes.Location = New System.Drawing.Point(192, 139)
+        Me.btnVezes.Name = "btnVezes"
+        Me.btnVezes.Size = New System.Drawing.Size(75, 23)
+        Me.btnVezes.TabIndex = 7
+        Me.btnVezes.Text = "VEZES"
+        Me.btnVezes.UseVisualStyleBackColor = True
+        '
+        'btnDividir
+        '
+        Me.btnDividir.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnDividir.Location = New System.Drawing.Point(273, 139)
+        Me.btnDividir.Name = "btnDividir"
+        Me.btnDividir.Size = New System.Drawing.Size(75, 23)
+        Me.btnDividir.TabIndex = 8
+        Me.btnDividir.Text = "DIVIDIR"
+        Me.btnDividir.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(29, 233)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(104, 21)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "RESULTADO:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(61, 284)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(243, 15)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Desenvolvido e criado por Daniel Damaceno."
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(373, 302)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.btnDividir)
+        Me.Controls.Add(Me.btnVezes)
+        Me.Controls.Add(Me.btnMenos)
+        Me.Controls.Add(Me.btnSoma)
+        Me.Controls.Add(Me.lblResultado)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtNumero2)
+        Me.Controls.Add(Me.txtNumero1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Calculadora do Daniel"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtNumero1 As TextBox
+    Friend WithEvents txtNumero2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblResultado As Label
+    Friend WithEvents btnSoma As Button
+    Friend WithEvents btnMenos As Button
+    Friend WithEvents btnVezes As Button
+    Friend WithEvents btnDividir As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ CALCULADORA/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnSoma.Click
+        lblResultado.Text = Convert.ToInt32(txtNumero1.Text) + Convert.ToInt32(txtNumero2.Text)
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnMenos_Click(sender As Object, e As EventArgs) Handles btnMenos.Click
+        lblResultado.Text = Convert.ToInt32(txtNumero1.Text) - Convert.ToInt32(txtNumero2.Text)
+    End Sub
+
+    Private Sub btnVezes_Click(sender As Object, e As EventArgs) Handles btnVezes.Click
+        lblResultado.Text = Convert.ToInt32(txtNumero1.Text) * Convert.ToInt32(txtNumero2.Text)
+    End Sub
+
+    Private Sub btnDividir_Click(sender As Object, e As EventArgs) Handles btnDividir.Click
+        lblResultado.Text = Convert.ToInt32(txtNumero1.Text) / Convert.ToInt32(txtNumero2.Text)
+    End Sub
+End Class
